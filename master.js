860a4bb4e2d88cdbf197ac75f7beb746986fa511
kevin von flotow <vonflow@gmail.com>
2014-06-04 20:39:25 -0500

    switch to spaces

--- master.js
+++ master.js
@@ -5,51 +5,51 @@
  *  of CPUs and reloads them on crash
  */
  ( function ()
-	{
-		var CLUSTER = require( 'cluster' )
-		
-		if ( !CLUSTER.isMaster )
-		{
-			return
-		}
-		
-		var OS = require( 'os' )
-		
-		var NUM_CPUS = OS.cpus().length || 1
-		
-		// lock at 2 threads for now
-		for ( var i = 0; i < 2; ++i )
-		{
-			var worker = CLUSTER.fork()
-			
-			worker
-				.on( 'listening', function ( address )
-					{
-						// worker listening
+    {
+        var CLUSTER = require( 'cluster' )
+        
+        if ( !CLUSTER.isMaster )
+        {
+            return
+        }
+        
+        var OS = require( 'os' )
+        
+        var NUM_CPUS = OS.cpus().length || 1
+        
+        // lock at 2 threads for now
+        for ( var i = 0; i < 2; ++i )
+        {
+            var worker = CLUSTER.fork()
+            
+            worker
+                .on( 'listening', function ( address )
+                    {
+                        // worker listening
 
-					}
-				)
-				.on( 'exit', function ( code, signal )
-					{
-						if ( signal )
-						{
-							console.log( 'worker was killed by signal: ' + signal )
-							
-							// should we fork a new worker here?
-						}
-						else if ( code !== 0 )
-						{
-							console.log( 'worker exited with error code: ' + code )
-							
-							// worker crashed, fork a new one
-							CLUSTER.fork()
-						}
-						else
-						{
-							console.log( 'worker success!' )
-						}
-					}
-				)
-		}
-	}
+                    }
+                )
+                .on( 'exit', function ( code, signal )
+                    {
+                        if ( signal )
+                        {
+                            console.log( 'worker was killed by signal: ' + signal )
+                            
+                            // should we fork a new worker here?
+                        }
+                        else if ( code !== 0 )
+                        {
+                            console.log( 'worker exited with error code: ' + code )
+                            
+                            // worker crashed, fork a new one
+                            CLUSTER.fork()
+                        }
+                        else
+                        {
+                            console.log( 'worker success!' )
+                        }
+                    }
+                )
+        }
+    }
 )()
--- worker.js
+++ worker.js
@@ -2,66 +2,66 @@
  *  2014 kevin von flotow
  */
 ( function ()
-	{
-		var CLUSTER = require( 'cluster' )
-		
-		// return if not worker
-		if ( !CLUSTER.isWorker )
-		{
-			return
-		}
-		
-		var PATH = require( 'path' )
+    {
+        var CLUSTER = require( 'cluster' )
+        
+        // return if not worker
+        if ( !CLUSTER.isWorker )
+        {
+            return
+        }
+        
+        var PATH = require( 'path' )
 
-		// load net module for tcp server
-		var NET = require( 'net' )
+        // load net module for tcp server
+        var NET = require( 'net' )
 
-		// require index from lib directory
-		var LIB = require( PATH.join( __dirname, 'lib' ) )
+        // require index from lib directory
+        var LIB = require( PATH.join( __dirname, 'lib' ) )
 
-		// reference Socket class
-		var Socket = LIB.Socket
+        // reference Socket class
+        var Socket = LIB.Socket
 
-		// default port is 2324 - can overwrite with command line
-		var PORT = 2324
+        // default port is 2324 - can overwrite with command line
+        var PORT = 2324
 
-		// set custom port from command line
-		if ( process.argv.length > 2 )
-		{
-			// use the first argument
-			PORT = process.argv[ 2 ]
-		}
+        // set custom port from command line
+        if ( process.argv.length > 2 )
+        {
+            // use the first argument
+            PORT = process.argv[ 2 ]
+        }
 
-		// create tcp server
-		var server = NET.createServer(
-			{
-				allowHalfOpen: true
-			}
-		)
+        // create tcp server
+        var server = NET.createServer(
+            {
+                allowHalfOpen: true
+            }
+        )
 
-		// listen for connections
-		server.on( 'connection', function ( socket )
-			{
-				new Socket( socket )
-			}
-		)
+        // listen for connections
+        server.on( 'connection', function ( socket )
+            {
+                new Socket( socket )
+            }
+        )
 
-		// bind server to port
-		server.listen( PORT, function ()
-			{
-				// server is bound
-				console.log( 'nodebee worker listening on port ' + PORT )
-			}
-		)
-		
-		/* process.on( 'uncaughtException', function ( msg, data )
-			{
-				console.log( msg )
-				
-				console.log( data )
-				
-				process.kill()
-			}
-		) */
-	}
+        // bind server to port
+        server.listen( PORT, function ()
+            {
+                // server is bound
+                console.log( 'nodebee worker listening on port ' + PORT )
+            }
+        )
+        
+        /* process.on( 'uncaughtException', function ( msg, data )
+            {
+                console.log( msg )
+                
+                console.log( data )
+                
+                process.kill()
+            }
+        ) */
+    }
 )()
